5991eb005fe9f174217e77a3ada2ca51d2e85912
Angel Byun <abyun22@turing.edu>
2023-03-08 09:36:45 -0700

    Create an empty array for the attribute food_trucks and then will create a method to add each individually food_truck into that array

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe Event do
     expect(event).to be_an(Event)
     expect(event.name).to eq("South Pearl Street Farmers Market")
   end
+
+  it 'will have an empty array for food_trucks' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    event = Event.new("South Pearl Street Farmers Market")
+
+    expect(event.food_trucks).to eq([])
+  end
+
+  it 'will add vendors' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    event = Event.new("South Pearl Street Farmers Market")
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+  end
 end
